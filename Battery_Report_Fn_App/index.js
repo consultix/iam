4e3275beb20461e56784b1529ad1b28f5ffb6784
aymanhendawy <ayman.hendawy@gmail.com>
2017-10-18 18:35:14 +0200

    modify battery table to reflect current status only not history

--- Battery_Report_Fn_App/index.js
+++ Battery_Report_Fn_App/index.js
@@ -1,10 +1,6 @@
 
 module.exports = function (context, eventHubMessages) {
-    // context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
-    
-    // eventHubMessages.forEach(message => {
-    //     context.log(`Processed message ${message}`);
-    // });
+
     var azure               = require('azure-storage');
     var batterytable           = 'BatteryTable';  
     var alarm_tablename     = 'BatteryAlarmTable';  
@@ -56,6 +52,62 @@ module.exports = function (context, eventHubMessages) {
     }
 
 
+    function add_tableentr(item, table)
+    {
+        var date = Date.now();
+        var partitionKey = Math.floor(date / (24 * 60 * 60 * 1000)) + '';
+        var rowKey       = date ++;
+
+        var entGen = azure.TableUtilities.entityGenerator;
+    
+        var entr = {
+            PartitionKey  : entGen.String(partitionKey),
+            RowKey        : entGen.String(rowKey),
+            devID         : entGen.String(item.ID),
+            //BatteryVolt   : entGen.DOUBLE(item.batt_volt),
+            //BatteryLevel  : entGen.INT32(item.batt_level),
+        };
+        
+        table.push(entr);
+    }
+        
+
+
+
+
+    tableService.queryEntities(batterytable, query, null, function(error, result, response) 
+    {
+       if(!error) 
+       {    
+           //delete all entries
+            var queryentr     =  result.entries; 
+            queryentr.forEach(function(item)
+            {
+                tableService.deleteEntity('batterytable', item, function(error, response){
+                    if(!error) {
+                        context.log('Entries Deleted');
+                    }
+                 });
+            });
+
+            //Form the new table
+            var tableentr = [];
+            event_msg.forEach(function(item){
+                if(item.batt_level )//ON/OFF antenna alarm packet
+                    add_tableentr(item, tableentr);     
+                else
+                    return;
+            });
+
+            //Insert the new table
+            // tableService.insertEntity(batterytable, tableentr[0], function (error, result, response) {
+            //     if(!error){
+            //         context.log('Entities inserted');
+            //     } 
+            // });
+        }
+    }); 
+
     var findentr = 
     {
          devid : {'_': 0},
@@ -69,109 +121,109 @@ module.exports = function (context, eventHubMessages) {
       return JSON.stringify(entr.devID) == JSON.stringify(findentr.devid);
     } 
 
+    // tableService.queryEntities(batterytable, query, null, function(error, result, response) 
+    // {
+    //    if(!error) 
+    //    {      
+    //         var queryentr     =  result.entries; 
+    //         queryentr.reverse(); 
 
-    tableService.queryEntities(batterytable, query, null, function(error, result, response) 
-    {
-       if(!error) 
-       {          
-            var queryentr     =  result.entries; 
-            queryentr.reverse(); 
+    //         event_msg.forEach(function(item){
 
-            event_msg.forEach(function(item){
+    //             if(item.batt_level )//ON/OFF antenna alarm packet
+    //             ;  
+    //             else
+    //               return;
 
-                if(item.batt_level )//ON/OFF antenna alarm packet
-                ;  
-                else
-                  return;
+    //             findentr.devid._  = item.ID;
 
-                findentr.devid._  = item.ID;
-
-                var indx = queryentr.findIndex(ishere);
+    //             var indx = queryentr.findIndex(ishere);
                 
-                if(indx < 0)
-                {
-                    context.log('Entry Doesnt exist, we will add it');
-                    tablestrg_add_msg(item, batterytable);
-                }
-                else
-                {
-                    queryentr[indx].BatteryVolt._  =  item.batt_volt;
-                    queryentr[indx].BatteryLevel._ =  item.batt_level;
+    //             if(indx < 0)
+    //             {
+    //                 context.log('Entry Doesnt exist, we will add it');
+    //                 tablestrg_add_msg(item, batterytable);
+    //             }
+    //             else
+    //             {
+    //                 queryentr[indx].BatteryVolt._  =  item.batt_volt;
+    //                 queryentr[indx].BatteryLevel._ =  item.batt_level;
                     
-                    tableService.replaceEntity(batterytable, queryentr[indx], function(error, result, response)
-                    {
-                        if(!error) {
-                            context.log('Entry updated ' );
-                        }
-                    });
-                }
-            });
-        }
-    });  
-     
-    tableService.queryEntities(batterytable, query, null, function(error, result, response) 
-    {
-       if(!error) 
-       { 
-            var queryentr     =  result.entries;  
-            queryentr.forEach(function(item)
-            {
-                var entrtime = Math.floor(Date.parse(item.Timestamp._)/1000/60);
-                var currtime = Math.floor(Date.now()/1000/60);
-                if(entrtime != currtime)
-                     tableService.deleteEntity('batterytable', item, function(error, response){
-                        if(!error) {
-                            context.log('Entry not updated so deleted' );
-                        }
-                    });
+    //                 tableService.replaceEntity(batterytable, queryentr[indx], function(error, result, response)
+    //                 {
+    //                     if(!error) {
+    //                         context.log('Entry updated ' );
+    //                     }
+    //                 });
+    //             }
+    //         });
+    //     }
+    // });  
 
-            });
-       }
-    });
+    //deleete entries which is not updated
+    // tableService.queryEntities(batterytable, query, null, function(error, result, response) 
+    // {
+    //    if(!error) 
+    //    { 
+    //         var queryentr     =  result.entries;  
+    //         queryentr.forEach(function(item)
+    //         {
+    //             var entrtime = Math.floor(Date.parse(item.Timestamp._)/1000/60);
+    //             var currtime = Math.floor(Date.now()/1000/60);
+    //             if(entrtime != currtime)
+    //                  tableService.deleteEntity('batterytable', item, function(error, response){
+    //                     if(!error) {
+    //                         context.log('Entry not updated so deleted' );
+    //                     }
+    //                 });
+
+    //         });
+    //    }
+    // });
     
-    tableService.queryEntities(alarm_tablename, query, null, function(error, result, response) 
-    {
-       if(!error) 
-       {          
-            var queryentr     =  result.entries; 
-            queryentr.reverse(); 
+    // tableService.queryEntities(alarm_tablename, query, null, function(error, result, response) 
+    // {
+    //    if(!error) 
+    //    {          
+    //         var queryentr     =  result.entries; 
+    //         queryentr.reverse(); 
 
-            event_msg.forEach(function(item){
+    //         event_msg.forEach(function(item){
 
-                if(item.batt_level )//ON/OFF antenna alarm packet
-                ;  
-                else
-                  return;
+    //             if(item.batt_level )//ON/OFF antenna alarm packet
+    //             ;  
+    //             else
+    //               return;
                 
-                // if below alarm battery threshold so battery level is low
-                // and need to update alarm battery table
-                if(item.batt_level > alarm_batt_level)
-                   return;
+    //             // if below alarm battery threshold so battery level is low
+    //             // and need to update alarm battery table
+    //             if(item.batt_level > alarm_batt_level)
+    //                return;
 
-                findentr.devid._  = item.ID;
+    //             findentr.devid._  = item.ID;
 
-                var indx = queryentr.findIndex(ishere);
+    //             var indx = queryentr.findIndex(ishere);
                 
-                if(indx < 0)
-                {
-                    context.log('Entry Doesnt exist, we will add it');
-                    tablestrg_add_msg(item, alarm_tablename);
-                }
-                else
-                {
-                    queryentr[indx].BatteryVolt._  =  item.batt_volt;
-                    queryentr[indx].BatteryLevel._ =  item.batt_level;
+    //             if(indx < 0)
+    //             {
+    //                 context.log('Entry Doesnt exist, we will add it');
+    //                 tablestrg_add_msg(item, alarm_tablename);
+    //             }
+    //             else
+    //             {
+    //                 queryentr[indx].BatteryVolt._  =  item.batt_volt;
+    //                 queryentr[indx].BatteryLevel._ =  item.batt_level;
                     
-                    tableService.replaceEntity(alarm_tablename, queryentr[indx], function(error, result, response)
-                    {
-                        if(!error) {
-                            context.log('Entry updated ' );
-                        }
-                    });
-                }
-            });
-        }
-    });    
+    //                 tableService.replaceEntity(alarm_tablename, queryentr[indx], function(error, result, response)
+    //                 {
+    //                     if(!error) {
+    //                         context.log('Entry updated ' );
+    //                     }
+    //                 });
+    //             }
+    //         });
+    //     }
+    // });    
     
 
     context.done();
