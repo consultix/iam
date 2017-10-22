c4a94c8e7def12bdbd732ca1e82d6422b19613b3
aymanhendawy <ayman.hendawy@gmail.com>
2017-10-22 16:47:57 +0200

    nodescurrentstatus updated, now we delete and add with every new message

--- Nodes_Current_State_Report_Fn_App/index.js
+++ Nodes_Current_State_Report_Fn_App/index.js
@@ -1,14 +1,82 @@
-module.exports = function (context, eventHubMessages) {
-    context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
-    
-    // eventHubMessages.forEach(message => {
-    //     context.log(`Processed message ${message}`);
-    // });
+var azure               = require('azure-storage');
+var connectionString    = 'DefaultEndpointsProtocol=https;AccountName=butterflystorageaccount;AccountKey=M2fwzoGsZ+nlxeKY8wRDlCjXr/YUPkJHFG9cuX0ve3DVYyvugi0lNNOamWV+E45WXQn4kCyigCT9i1+oFbI1QQ==;EndpointSuffix=core.windows.net';
+var tableService        = azure.createTableService(connectionString);
 
-    var azure               = require('azure-storage');
+
+function add_tableentr(item, table)
+{
+    var date = Date.now();
+    var partitionKey = Math.floor(date / (24 * 60 * 60 * 1000)) + '';
+
+    var entGen = azure.TableUtilities.entityGenerator;
+
+    var entr = {
+        PartitionKey  : entGen.String(partitionKey),
+        RowKey        : entGen.String(item.ID),
+        devID         : entGen.String(item.ID),
+        Status        : entGen.Boolean(item.Pin1)
+    };
+    
+    table.push(entr);
+}
+
+
+function table_update_currentstatus(table, entries, context)
+{
+    tableService.createTableIfNotExists(table, function(error, result, response) {
+        if (error) {
+          context.log("Error Creating ", table );
+        }
+     }); 
+
+    var query = new azure.TableQuery()
+
+    tableService.queryEntities(table, query, null, function(error, result, response) 
+    {
+        if(!error) 
+        {    
+            var batch = new azure.TableBatch();
+
+            //delete all entries
+            var queryentr     =  result.entries; 
+            queryentr.forEach(function(item)
+            {
+                batch.deleteEntity(item, {echoContent: true});
+            });
+
+            if(batch.hasOperations())
+            {
+                tableService.executeBatch(table, batch, function (error, result, response) {
+                    if(!error) 
+                        context.log('Entities Deleted');
+            
+                    else
+                        context.log(table,'**** Error Deleting Entries***', error.code);
+                });
+
+                batch.clear();
+            }   
+
+            //Insert the new table
+            entries.forEach(function(item){
+                batch.insertEntity(item, {echoContent: true});
+            });    
+
+            tableService.executeBatch(table, batch, function (error, result, response) {
+                if(!error) 
+                    context.log('Entities inserted');
+        
+                else
+                    context.log(table,'**** Error Inserting Entries***', error.code);
+            });
+        }
+    });
+}
+
+
+module.exports = function (context, eventHubMessages) {
+
     var tablename           = 'NodesCurrentStatusTable';  
-    var connectionString    = 'DefaultEndpointsProtocol=https;AccountName=butterflystorageaccount;AccountKey=M2fwzoGsZ+nlxeKY8wRDlCjXr/YUPkJHFG9cuX0ve3DVYyvugi0lNNOamWV+E45WXQn4kCyigCT9i1+oFbI1QQ==;EndpointSuffix=core.windows.net';
-    var tableService        = azure.createTableService(connectionString);
     var date                = Date.now();
 
     if(typeof eventHubMessages === 'string')
@@ -19,72 +87,18 @@ module.exports = function (context, eventHubMessages) {
  
         //var event_msg = [{"projectname":"Butterfly","ID":"403d9c26e44f4078","Pin0":1,"Pin1":1}];
 
-    tableService.createTableIfNotExists(tablename, function(error, result, response) {
-       if (error) {
-         context.log("Error Creating ", tablename );
-       }
-    }); 
-    
-    function tablestrg_add_msg(msg, table)
-    {
-        var date = Date.now();
-        var partitionKey = Math.floor(date / (24 * 60 * 60 * 1000)) + '';
-        var rowKey       = date + '';
 
-        var entGen = azure.TableUtilities.entityGenerator;
-    
-        var tableentr = {
-            PartitionKey  : entGen.String(partitionKey),
-            RowKey        : entGen.String(rowKey),
-            devID         : entGen.String(msg.ID),
-            Status        : msg.Pin1,
-            Date          : date
-        };
+    //Constract the new table
+    var tableentr = [];
+    event_msg.forEach(function(item){
+        if(item.batt_level )//ON/OFF antenna alarm packet
+            return;
+        else
+            add_tableentr(item, tableentr);     
+    });
 
-        tableService.insertEntity(table, tableentr, function (error, result, response) {
-            if(!error){
-                context.log('Entity inserted');
-            }
-        });
-    }
-
-    tableService.queryEntities(tablename, null, null, function(error, result, response) 
-    {
-       if(!error) 
-       {          
-            var queryentr     =  result.entries; 
-            queryentr.reverse(); 
-
-            event_msg.forEach(function(item){
-
-                if(item.batt_level )//ON/OFF antenna alarm packet
-                    return;
-
-                var indx = queryentr.findIndex(function (entr)
-                {
-                    return entr.devID._ == item.ID;
-                });
-                
-                if(indx < 0)
-                {
-                    context.log('Entry Doesnt exist, we will add it');
-                    tablestrg_add_msg(item, tablename);
-                }
-                else
-                {
-                    queryentr[indx].Status._  =  item.Pin1;
-                    queryentr[indx].Date._    =  date;
-                    
-                    tableService.replaceEntity(tablename, queryentr[indx], function(error, result, response)
-                    {
-                        if(!error) {
-                            context.log('Entry updated ' );
-                        }
-                    });
-                }
-            });
-        }
-    });   
+    if(tableentr.length)
+        table_update_currentstatus(tablename, tableentr, context);        
 
     context.done();
 };
