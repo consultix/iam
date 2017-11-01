b4982cdcbf46fa79c1cc959f1fc9d7d8de50ce28
aymanhendawy <ayman.hendawy@gmail.com>
2017-11-01 18:32:48 +0200

    update availability fn

--- Availability_Report_Fn_App/index.js
+++ Availability_Report_Fn_App/index.js
@@ -12,55 +12,55 @@ function groupby_devID(groups, item)
 }
 
 
-var grouped_devIDs_period = new Object();
-
-function Calc_Acc_Periods(table, starttime, state)
+function Calc_Acc_Periods(table, starttime, state, callback)
 {
-    
+    //var grouped_devIDs_period ={} ;
     var query = new azure.TableQuery()
-        .where('status == ?', state);   
+        .where('status == ?', state);  
+        
+    var testarr = [];
 
     tableService.queryEntities(table, query, null, function(error, result, response) 
-    {
-        if(!error) 
-        {   
-            var queryentr     =  result.entries; 
-            queryentr.reverse();
-            
-            //Fitler according to time window/////////////////////////////////////////
-            //check lastseen time if more than start_time_to_check consider this row           
-            var lastseen_filterd = queryentr.filter(function(entr){
-                var lastseen = Date.parse(entr.lastseen._);
-                return (lastseen > starttime);
-            }, starttime);
+        {
+            if(!error) 
+            {   
+                var queryentr     =  result.entries; 
+                queryentr.reverse();
+                
+                //Fitler according to time window/////////////////////////////////////////
+                //check lastseen time if more than start_time_to_check consider this row           
+                var lastseen_filterd = queryentr.filter(function(entr){
+                    var lastseen = Date.parse(entr.lastseen._);
+                    return (lastseen > starttime);
+                }, starttime);
 
 
-            lastseen_filterd.forEach(function(entr){
-                var start = Date.parse(entr.start._);
-                if(start < starttime)
-                {
-                    start = starttime;
-                    entr.period._ = Date.parse(entr.lastseen._) -  start;
-                }
-            }, starttime);
-            /////////////////////////////////////////////////////////////////////////
+                lastseen_filterd.forEach(function(entr){
+                    var start = Date.parse(entr.start._);
+                    if(start < starttime)
+                    {
+                        start = starttime;
+                        entr.period._ = Date.parse(entr.lastseen._) -  start;
+                    }
+                }, starttime);
+                /////////////////////////////////////////////////////////////////////////
 
-            //Grouping devIDs, then calc. sum of periods/////////////////////////////
-            //Grouping
-            var grouped_devIDs = lastseen_filterd.reduce(groupby_devID, {});
-            
-            
-            //Calc sum of periods
-            Object.keys(grouped_devIDs).forEach(function (key){
-                grouped_devIDs_period[key] = grouped_devIDs[key].reduce(function(sum_period, item){
-                    
-                    return sum_period + item.period._; 
-                },0);
-            });
-            ////////////////////////////////////////////////////////////////////////
-        }
-    });
-    return grouped_devIDs_period;
+                //Grouping devIDs, then calc. sum of periods/////////////////////////////
+                //Grouping
+                var grouped_devIDs = lastseen_filterd.reduce(groupby_devID, {});
+                
+                var grouped_devIDs_period = {};
+                //Calc sum of periods
+                Object.keys(grouped_devIDs).forEach(function (key){
+                    grouped_devIDs_period[key] = grouped_devIDs[key].reduce(function(sum_period, item){
+                        
+                        return sum_period + item.period._; 
+                    },0);
+                });
+                ////////////////////////////////////////////////////////////////////////
+            }
+            return callback(grouped_devIDs_period); 
+        });
 }
 
 
@@ -97,67 +97,69 @@ module.exports = function (context, myTimer) {
     var Events_History_table    = 'EventsHistoryTable';
     var Availability_table      = 'AvailabilityTable';
 
-    var ON_Periods = new Object(Calc_Acc_Periods(Events_History_table, start_time_to_check, 1));
-    
-    grouped_devIDs_period = [];
-    
-    var OFF_Peridos = new Object(Calc_Acc_Periods(Events_History_table, start_time_to_check, 0));
-
-    if(ON_Periods)
-    {
-    //Calc Availability/////////////////////////////////////////////////////
-    var availability = [];
-    Object.keys(ON_Periods).forEach(function(key){
-        var value = ON_Periods[key] * 100 / avail_period_in_ms;
-        availability.push({devID:key, avail:value});
-    }); 
-    ////////////////////////////////////////////////////////////////////////
-    
-    //Create, Add, update Availability Report///////////////////////////////
+    //Create Availability Report///////////////////////////////
     //Create Table if not exist
     tableService.createTableIfNotExists(Availability_table, function(error, result, response) {
         if (error) {
             context.log("Error Creating ", Events_History_table );
         }
-    });           
+    }); 
+    ////////////////////////////////////////////////////////////////////////
 
-
-    //Check Availability Table
-    tableService.queryEntities(Availability_table, null, null, function(error, result, response) 
+    Calc_Acc_Periods(Events_History_table, start_time_to_check, 1, function(on_periods)
     {
-        if(!error) 
-        {          
-            var queryentr = result.entries; 
-            queryentr.reverse(); 
-            availability.forEach(function(item)
-            {
-                var indx = queryentr.findIndex(function (entr)
-                {
-                    return entr.devID._ == item.devID;
-                });
+        Calc_Acc_Periods(Events_History_table, start_time_to_check, 0, function(off_periods)
+        {
+            //Calc Availability/////////////////////////////////////////////////////
+            var availability = [];
+            Object.keys(on_periods).forEach(function(key){
+                var off_per;
+                if(off_periods[key]) off_per =  off_periods[key];
+                else off_per = 0;
+                var value = on_periods[key] * 100 / (on_periods[key] + off_per);
+                availability.push({devID:key, avail:value});
+            });
+            ////////////////////////////////////////////////////////////////////////
 
-                if(indx < 0)
-                {
-                    //Add new entry of devID not exist 
-                    tablestrg_add_msg(item, Availability_table); 
-                }
-                else
-                {
-                    //Update already existing entry
-                    queryentr[indx].availability._  = item.avail; 
-                    tableService.replaceEntity(Availability_table, queryentr[indx], function(error, result, response)
+            //Check Availability Table
+            tableService.queryEntities(Availability_table, null, null, function(error, result, response) 
+            {
+                if(!error) 
+                {          
+                    var queryentr = result.entries; 
+                    queryentr.reverse(); 
+
+                    availability.forEach(function(item)
                     {
-                        if(!error) {
-                            context.log(' Entity updated ' );
+                        var indx = queryentr.findIndex(function (entr)
+                        {
+                            return entr.devID._ == item.devID;
+                        });
+
+                        if(indx < 0)
+                        {
+                            //Add new entry of devID not exist 
+                            tablestrg_add_msg(item, Availability_table); 
+                        }
+                        else
+                        {
+                            //Update already existing entry
+                            queryentr[indx].availability._  = item.avail; 
+                            tableService.replaceEntity(Availability_table, queryentr[indx], function(error, result, response)
+                            {
+                                if(!error) {
+                                    context.log(' Entity updated ' );
+                                }
+                            });
                         }
                     });
                 }
-            });
-        }
-    }); 
-    ///////////////////////////////////////////////////////////////////////////
-}
-
+            }); 
+            ///////////////////////////////////////////////////////////////////////////
+        });
+    });    
+    
+          
     if(myTimer.isPastDue)
     {
         context.log('JavaScript is running late!');
